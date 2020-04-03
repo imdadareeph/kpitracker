c4c103b96e488176fbb19fa84cff070c5ea30515
imdadareeph <imdadareeph@gmail.com>
2020-04-04 04:29:49 +0530

    automatic data insertion on application start
    automatic data insertion on application start

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO `role` VALUES (1,'ADMIN');
+INSERT INTO `role` VALUES (2,'USER');
+
+INSERT INTO `user` VALUES (8,1,'imdadareeph@gmail.com','Imdad','$2a$10$DMgd3SKvldhp/a3vjOo/WuxEED6NBfKujLX8ef6vUoZH6pp56I04O',1),(9,1,'mohdfaizcs059@gmail.com','Faiz','$2a$10$TDd42umbjoc8V9d/qPYaVuJqpT.9i6Hpwz//O/IIJ18Jgzpxh4zZe',2),(10,1,'abhi.mca50@gmail.com','Abhijeet','$2a$10$HOXI6wPC3W8qB6jZWB2CPeMOwKo7gu7AhYrL6BsJyrfGDBPZgvCEO',2);
+
